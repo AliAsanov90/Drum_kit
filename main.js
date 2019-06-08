689b817d7aa6b1b217a8974554668592acab7542
Ali <aliasanov07@gmail.com>
2019-06-08 15:26:39 +0300

    finished

---
+++ main.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('keyup', playSound);
+function playSound(e) {
+  let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+let keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
